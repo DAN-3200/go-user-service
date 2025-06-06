07c964b0170bd3ef316d4234e47753b0bc432767
Daniel <danmoreira.dev@gmail.com>
2025-06-06 14:36:35 -0300

    fix: erros no jwt

--- internal/userauth/use_jwt.go
+++ internal/userauth/use_jwt.go
@@ -9,41 +9,39 @@ import (
 	"github.com/golang-jwt/jwt/v5"
 )
 
-var secretKEY = []byte(os.Getenv("secret_key"))
+var secretKEY = []byte(os.Getenv("SECRET_KEY"))
 
-func GenerateJWT(userID, userEmail, userRole string) (string, error) {
-
+func GenerateJWT(userID, userRole string) (string, error) {
 	// formato do JWT : Header.Payload.Signature
-	var tokenString, err = jwt.NewWithClaims(
-		jwt.SigningMethodHS256, // = Header
-
-		jwt.MapClaims{ // = Payload
-			"userID": userID,
-			"iss":    userEmail,
-			"role":   userRole,
-			"exp":    time.Now().Add(time.Hour * 2).Unix(), // expirar: tempo atual + 2 horas
-		},
-	).SignedString(secretKEY) // = Signature
+	tokenString, err := jwt.
+		NewWithClaims(
+			jwt.SigningMethodHS256, // = Header
+			jwt.MapClaims{ // = Payload
+				"userID": userID,
+				"iss":    "rest.user.service",
+				"role":   userRole,
+				"exp":    time.Now().Add(time.Hour * 2).Unix(), // expirar: tempo atual + 2 horas
+			},
+		).
+		SignedString(secretKEY) // = Signature
 
 	return tokenString, err
 }
 
 type ModelClaims struct {
 	UserID string
-	Iss    string
 	Role   string
+	Iss    string
 }
 
 func ValidateJWT(tokenString string) (bool, ModelClaims) {
 	tokenString = RemoveBearerPrefix(tokenString)
-
-	// Analisa o Token
-	var token, err = jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+	
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 		// Verificar se o método é do tipo criptográfico HS256
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return "Não é do tipo HMAC", nil
 		}
-		// utiliza a `secretKEY` para validação
 		return secretKEY, nil
 	})
 
@@ -55,15 +53,14 @@ func ValidateJWT(tokenString string) (bool, ModelClaims) {
 	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
 		return true, ModelClaims{
 			UserID: claims["userID"].(string),
-			Iss:    claims["iss"].(string),
 			Role:   claims["role"].(string),
+			Iss:    claims["iss"].(string),
 		}
 	} else {
 		return false, ModelClaims{}
 	}
 }
 
-// Remover o prefixo `Bearer`
 func RemoveBearerPrefix(TokenString string) string {
 	if strings.HasPrefix(TokenString, "Bearer ") {
 		TokenString = strings.TrimPrefix(TokenString, "Bearer ")
